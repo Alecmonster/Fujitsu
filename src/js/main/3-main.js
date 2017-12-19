a3970c5b3e1c60446824a1895574611665f2eb18
jopfre <jonathan@devconq.com>
2017-12-19 11:28:36 +0000

    slider mobile and arrows

--- src/js/main/3-main.js
+++ src/js/main/3-main.js
@@ -1,4 +1,15 @@
- $('.owl-carousel').owlCarousel({
+$('.owl-carousel').owlCarousel({
   nav: true,
-  dots: false
- });
+  navText: ['<img src="img/arrow-left.png">','<img src="img/arrow-right.png">'],
+  dots: false,
+  margin: 30,
+  responsiveClass:true,
+  responsive:{
+    0:{
+      items:1,
+    },
+    640:{
+      items:3,
+    }
+  }
+});
